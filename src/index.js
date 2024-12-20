758664c7649aed780f723da645198d24c252f527
RaquelCosta07 <raquel.s.gomes.costa@gmail.com>
2024-12-20 11:17:17 +0000

    finally the new DB worked

--- src/index.js
+++ src/index.js
@@ -8,7 +8,35 @@ const api = express();
 api.use(cors());
 api.use(express.json());
 
-const port = 5001;
+//funcion p conectar a la DB
+async function getDBConnection(){
+     const connection = await mysql.createConnection({
+        host: "mysql-3c94dc8c-series.l.aivencloud.com",
+        user: "avnadmin",
+        password: "AVNS_KHvsf58dWWj8arLDT4C",
+        database: "series_db",
+        port: 14105
+    });
+    connection.connect();
+    return connection;
+}
+
+const port = 4001;
 api.listen(port, () => {
     console.log(`Server is running. Go to http://localhost:${port}`);
+})
+
+
+//endpoints
+
+/*
+
+*/
+api.get("/api/series", async (req, res) => {
+    const connection = await getDBConnection();
+    const query = "SELECT * FROM series";
+    const [result] = await connection.query(query);
+    console.log(result);
+
+    res.json({});
 })
